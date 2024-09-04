41a589fe085b6a4aafff82598feb08246f217f60
Govind S B <b.s.dnivog@gmail.com>
2024-09-04 16:43:59 +0530

    moved to entrypoint script to download data

---
+++ main_app/entrypoint.sh
@@ -0,0 +1,67 @@
+# Get the dataset size from the environment variable
+data=${DATASET_SOURCE}
+
+if [ -z "$data" ]; then
+  echo "[ERROR]: Missing DATASET_SIZE environment variable"
+  exit 1 # Exit script if the environment variable is not set
+fi
+
+# Check if the previous dataset source is the same as the current one
+prev_data=$(cat data/prev_DATASET_SOURCE)
+
+dataset_state_change=false
+if [ "$prev_data" == "$data" ]; then
+  echo "The dataset source has not changed. Moving directly to run webshop."
+else
+  echo "The dataset source has changed from $prev_data to $data. Proceeding with the script."
+  echo "$data" > data/prev_DATASET_SOURCE
+  dataset_state_change=true
+fi
+
+if [ "$dataset_state_change" = false ]; then
+
+  # Change to the data directory
+  cd data
+
+  # Check if the file items_human_ins.json exists, if not download it
+  if [ ! -f "items_human_ins.json" ]; then
+    gdown https://drive.google.com/uc?id=14Kb5SPBk_jfdLZ_CDBNitW98QLDlKR5O
+  fi
+
+  # Define a function to download a file if it does not exist
+  download_if_not_exists() {
+    local file=$1
+    local url=$2
+    if [ ! -f "$file" ]; then
+      gdown $url
+    fi
+  }
+
+  # Switch case for different dataset sources
+  case "$data" in
+    "small")
+      download_if_not_exists "items_shuffle_1000.json" "https://drive.google.com/uc?id=1EgHdxQ_YxqIQlvvq5iKlCrkEKR6-j0Ib"
+      download_if_not_exists "items_ins_v2_1000.json" "https://drive.google.com/uc?id=1IduG0xl544V_A_jv3tHXC0kyFi7PnyBu"
+      ;;
+    "all")
+      download_if_not_exists "items_ins_v2.json" "https://drive.google.com/uc?id=1s2j6NgHljiZzQNL3veZaAiyW_qDEgBNi"
+      download_if_not_exists "items_shuffle.json" "https://drive.google.com/uc?id=1s2j6NgHljiZzQNL3veZaAiyW_qDEgBNi"
+      ;;
+    *)
+      echo "[ERROR]: Invalid DATASET_SOURCE value"
+      exit 1
+      ;;
+  esac
+
+  # Change back to the previous directory
+  cd ..
+
+  # Build search engine index
+  cd search_engine
+  python convert_product_file_format.py # convert items.json => required doc format
+  ./run_indexing.sh
+  cd ..
+
+fi
+
+./run_dev.sh
--- main_app/setup.sh
+++ main_app/setup.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# Displays information on how to use script
-helpFunction()
-{
-  echo "Usage: $0 [-d small|all]"
-  echo -e "\t-d small|all - Specify whether to download entire dataset (all) or just 1000 (small)"
-  exit 1 # Exit script after printing help
-}
-
-# Get values of command line flags
-while getopts d: flag
-do
-  case "${flag}" in
-    d) data=${OPTARG};;
-  esac
-done
-
-if [ -z "$data" ]; then
-  echo "[ERROR]: Missing -d flag"
-  helpFunction
-fi
-
 # Install Python Dependencies via conda without prompts
 conda install -y spacy
 
@@ -31,32 +10,8 @@ pip install --no-input -r requirements_arm.txt
 conda install -y -c pytorch faiss-cpu
 conda install -y -c conda-forge openjdk=11
 
-# Helper function to download file if it doesn't exist
-download_if_not_exists() {
-  local url="$1"
-  local filename="$2"
-  if [ ! -f "$filename" ]; then
-    echo "Downloading $filename..."
-    gdown "$url" -O "$filename"
-  else
-    echo "File $filename already exists, skipping download."
-  fi
-}
-
 mkdir -p data;
-cd data;
-if [ "$data" == "small" ]; then
-  download_if_not_exists "https://drive.google.com/uc?id=1EgHdxQ_YxqIQlvvq5iKlCrkEKR6-j0Ib" "items_shuffle_1000.json"
-  download_if_not_exists "https://drive.google.com/uc?id=1IduG0xl544V_A_jv3tHXC0kyFi7PnyBu" "items_ins_v2_1000.json"
-elif [ "$data" == "all" ]; then
-  download_if_not_exists "https://drive.google.com/uc?id=1A2whVgOO0euk5O13n2iYDM0bQRkkRduB" "items_shuffle.json"
-  download_if_not_exists "https://drive.google.com/uc?id=1s2j6NgHljiZzQNL3veZaAiyW_qDEgBNi" "items_ins_v2.json"
-else
-  echo "[ERROR]: argument for \`-d\` flag not recognized"
-  helpFunction
-fi
-download_if_not_exists "https://drive.google.com/uc?id=14Kb5SPBk_jfdLZ_CDBNitW98QLDlKR5O" "items_human_ins.json"
-cd ..
+echo "none" > data/prev_DATASET_SOURCE
 
 # Download spaCy large NLP model
 python -m spacy download en_core_web_sm
@@ -64,9 +19,7 @@ python -m spacy download en_core_web_sm
 # Build search engine index
 cd search_engine
 mkdir -p resources resources_100 resources_1k resources_100k
-python convert_product_file_format.py # convert items.json => required doc format
 mkdir -p indexes
-./run_indexing.sh
 cd ..
 
 mkdir -p user_session_logs/
